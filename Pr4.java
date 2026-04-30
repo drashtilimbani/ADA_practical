87e3c0dbf7dd3500dde313daa8a4ec32c8f767df
drashtilimbani <drashtilimbani801@gmail.com>
2026-04-30 17:29:22 +0530

    Pr4
    Implement merge sort algorithm and measure execution time.

---
+++ Pr4.java
@@ -0,0 +1,41 @@
+import random
+import time
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
+size = 1000000
+data = [random.randint(1, 1000000) for _ in range(size)]
+
+start = time.time()
+merge_sort(data)
+end = time.time()
+
+print("Execution Time:", end - start)
